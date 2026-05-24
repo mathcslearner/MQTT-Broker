5b80f2c699d1f28555abb0c1bc213e45b134baff
mathcslearner <143976495+mathcslearner@users.noreply.github.com>
2026-05-24 19:27:14 -0400

    feat: add remaining function defs to mqtt.h and mqtt.c

--- src/mqtt.h
+++ src/mqtt.h
@@ -148,4 +148,16 @@ union mqtt_packet {
     struct mqtt_unsubscribe unsubscribe;
 };
 
+int mqtt_encode_length(unsigned char *, size_t);
+unsigned long long mqtt_decode_length(const unsigned char **);
+int unpack_mqtt_packet(const unsigned char *, union mqtt_packet *);
+unsigned char *pack_mqtt_packet(const union mqtt_packet *, unsigned);
+
+union mqtt_header *mqtt_packet_header(unsigned char);
+struct mqtt_ack *mqtt_packet_ack(unsigned char , unsigned short);
+struct mqtt_connack *mqtt_packet_connack(unsigned char, unsigned char, unsigned char);
+struct mqtt_suback *mqtt_packet_suback(unsigned char, unsigned short, unsigned char *, unsigned short);
+struct mqtt_publish *mqtt_packet_publish(unsigned char, unsigned short, size_t, unsigned char *, size_t, unsigned char *);
+void mqtt_packet_release(union mqtt_packet *, unsigned);
+
 #endif
