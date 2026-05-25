f005dbc871bc3244c79441a16e1836e270345271
mathcslearner <143976495+mathcslearner@users.noreply.github.com>
2026-05-24 23:04:39 -0400

    feat: encode/decode fcts for remaining length on MQTT packet header

--- src/mqtt.c
+++ src/mqtt.c
@@ -1,6 +1,56 @@
 #include <stdlib.h>
 #include <string.h>
 #include "mqtt.h"
+#include "pack.h"
+
+/*
+ * MQTT v3.1.1 standard: Remaining length field on the fixed header
+ * can be at most 4 bytes.
+ */
+static const int MAX_LEN_BYTES = 4;
+
+/*
+ * Encode Remaining Length on a MQTT packet header, comprised of Variable
+ * Header and Payload if present. It does not take into account the bytes
+ * required to store itself. Refer to MQTT v3.1.1 algorithm for the
+ * implementation
+ */
+int mqtt_encode_length(unsigned char *buf, size_t len) {
+    int bytes = 0;
+    do {
+        if (bytes + 1 > MAX_LEN_BYTES) {
+            return bytes;
+        }
+        short d = len % 128;
+        len /= 128;
+        /* If there are more digits to encode, set the top bit of this digit*/
+        if (len > 0) {
+            d |= 128;
+        }
+        buf[bytes++] = d;
+    } while (len > 0);
+    return bytes;
+}
+
+/*
+ * Decode Remaining Length comprised of Variable Header and Payload if
+ * present. It does not take into account the bytes for storing length. Refer
+ * to MQTT v3.1.1 algorithm for the implementation.
+ * 
+ * TODO: Handle case where multiplier > 128 * 128 * 128
+*/
+unsigned long long mqtt_decode_length(const unsigned char **buf) {
+    char c;
+    int multiplier = 1;
+    unsigned long long value = 0LL;
+    do {
+        c = **buf;
+        value += (c & 127) * multiplier;
+        multiplier *= 128;
+        (*buf)++;
+    } while ((c & 128) != 0);
+    return value;
+}
 
 static size_t unpack_mqtt_connect(const unsigned char *, union mqtt_header *, union mqtt_packet *);
 static size_t unpack_mqtt_publish(const unsigned char *, union mqtt_header *, union mqtt_packet *);
